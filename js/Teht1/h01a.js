2083ba86fceb31ea0033386bc2ec6b729229bf5e
Thorindar <50819779+Thorindar@users.noreply.github.com>
2019-05-20 09:28:22 +0300

    Testin aloitus
    Testin aloitus

---
+++ js/Teht1/h01a.js
@@ -0,0 +1,9 @@
+document.write("<br/>");
+document.write("h01a");
+document.write("<br/>");
+document.write("Hello World!");
+document.write("<br/>");
+document.write("h01b");
+document.write("</br>");
+document.write("Hei Maailma!");
+document.write("<br/>h01c<br/>Hallå Värld!");
